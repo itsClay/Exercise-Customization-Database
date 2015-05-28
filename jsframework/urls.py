041fbee0473f0f732e15914551ab4b0d5701dfb7
Clay Shaw <Claydshaw@gmail.com>
2015-05-28 16:13:34 -0700

    update settings and urls. add views. add static jquery / bootstraps.

--- jsframework/urls.py
+++ jsframework/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import include, url
-from django.contrib import admin
-from jsframework import urls as js_urls
+from . import views
 
-
+    # Examples:
 urlpatterns = [
     # Examples:
     # url(r'^$', 'django_drf_starter_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include(js_urls)),
+    url(r'^$', views.index)
 ]
