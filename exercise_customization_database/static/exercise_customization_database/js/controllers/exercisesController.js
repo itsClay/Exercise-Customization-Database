308fef9c917e2876ef1cdc16190fd080c9518bbb
Clay Shaw <Claydshaw@gmail.com>
2015-06-07 15:28:06 -0700

    exercise view and filter complete

--- exercise_customization_database/static/exercise_customization_database/js/controllers/exercisesController.js
+++ exercise_customization_database/static/exercise_customization_database/js/controllers/exercisesController.js
@@ -1,23 +1,8 @@
 exerciseApp.controller('exercisesController', function ($scope, $http) {
 
-	var exerciseList;
-
     $http.get('/api/exercises')
         .then(function(data) {
-            $scope.exerciseList = data;
-          
-
+            $scope.exerciseList = data.data;
+		    console.log($scope.exerciseList);
         });
-    console.log(exerciseList);
-
-	// $http.get('/api/exercises')
-	// 	.then(function(response) {
-	// 		$scope.exerciseList = response.data;
-	// 			console.log($scope.exerciseList)
-	// 	});
-
-	// console.log($scope.exerciseList)
-
-	// console.log(exerciseList.data)
-	// console.log(exerciseList.name)
 });
